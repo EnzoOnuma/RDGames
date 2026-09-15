36d8b40bf5df9eb1ff3acdeb0dec5def9599d995
Enzo <enzoonumaa400@gmail.com>
2026-09-15 19:26:36 -0300

    feat: Criando o primeiro código javascript do projeto

---
+++ src/js/script.js
@@ -0,0 +1,31 @@
+// ARRAY DE IMAGENS
+let imagens = ["src/assets/1.jpeg", "src/assets/2.jpeg", "src/assets/3.jpeg"];
+// POSIÇÃO QUE VAI INICIAR AS IMAGENS
+let index=0;
+// TEMPO PARA TROCAR AS IMAGENS
+let tempo = 3000; //3 segundos
+
+// FUNÇÃO DO SLIDESHOW
+function SlideShow(){
+    // DOM - PEGA O ID E PASSA O CAMINHO DAS IMAGENS
+    document.getElementById("imgBanner").src=imagens[index];
+    // INCREMENTO (++ = +1)
+    index++;
+
+    // ESTRUTURA CONDICIONAL IF
+    if(index == imagens.length){
+        index=0;
+    }
+    // MÉTODO SETTIMEOUT PARA EXECUTAR A FUNÇÃO E CHAMAR O TEMPO
+    setTimeout('SlideShow()', tempo)
+}
+// EXECUTANDO A FUNÇÃO
+SlideShow();
+
+const menuIcone = document.getElementById("menu-icone");
+const navMenu = document.getElementById("nav-menu");
+
+menuIcone.addEventListener('click',()=>{
+    navMenu.classList.toggle("active");
+    menuIcone.classList.toggle("open");
+})
